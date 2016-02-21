76fbc2b88b80ab0d444dc0eb9ce062289a77c203
Isaac Tian <isaac.tian@videoexpress.biz>
2016-02-21 12:45:04 +0800

    Add dht11 and remove lm35

--- sketch_humiture_DHT11/sketch_humiture_DHT11.ino
+++ sketch_humiture_DHT11/sketch_humiture_DHT11.ino
@@ -1,38 +1,77 @@
 #include <DHT.h>
 
 /*
- * DHT11温湿度计获取温湿度
- * 
- * 目的:通过读取DHT11模块的信号数据来读取温湿度数据并输出到串口
- * 
- * 前提:GND接地,VCC接入5v电源,信号接入到PIN 8
- * 
- */
+   DHT11温湿度计获取温湿度
+
+   目的:通过读取DHT11模块的信号数据来读取温湿度数据并输出到串口
+
+   前提:GND接地,VCC接入5v电源,信号接入到PIN 8
+
+*/
 
 #define PIN_DHT 8
 #define DHTTYPE DHT11
+struct Humiture {
+  float humidity;
+  float temperature;
+};
 
-DHT dht(PIN_DHT, DHTTYPE);
+class HumitureDHT11 {
+  public:
+    HumitureDHT11(int pin):dht(pin, DHT11) {
+      inputPin = pin;
+      pinMode(inputPin, INPUT);
+      (dht).begin();
+    }
+
+    Humiture readSensor() {
+      float humidity = (dht).readHumidity();
+      float temperature = (dht).readTemperature();
+      humiture.humidity = humidity;
+      humiture.temperature = temperature;
+      if (isnan(humidity)) {
+        Serial.println("Failed to read humidity from DHT sensor!");
+        humiture.humidity = -1;
+      }
+      if (isnan(temperature)) {
+        Serial.println("Failed to read temperature from DHT sensor!");
+        humiture.temperature = -1;
+      }
+      return humiture;
+    }
+
+  private:
+    int inputPin;
+    DHT dht;
+    Humiture humiture;
+};
+
+//DHT dht(PIN_DHT, DHTTYPE);
+HumitureDHT11 dht11(PIN_DHT);
 
 void setup() {
   Serial.begin(9600);
-  dht.begin();
-  
+  //dht.begin();
+  Serial.println("GO!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(2000);
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  delay(1000);
+  Serial.print("LOOP:");
+  /*float h = dht.readHumidity();
+    float t = dht.readTemperature();
 
-  if(isnan(h) || isnan(t)) {
+    if(isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
-    return;  
-  }
-  
+    return;
+    }*/
+
+  Humiture h = dht11.readSensor();
+  h.humidity = h.humidity;
+  h.temperature = h.temperature;
   Serial.print("Humidity = ");
-  Serial.print(h);
+  Serial.print(h.humidity);
   Serial.print(", Temp = ");
-  Serial.println(t);
+  Serial.println(h.temperature);
 }
--- sketch_octopus/sketch_octopus.ino
+++ sketch_octopus/sketch_octopus.ino
@@ -1,5 +1,41 @@
-#include <Wire.h> 
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <DHT.h>
+
+struct Humiture {
+  float humidity;
+  float temperature;
+};
+
+class HumitureDHT11 {
+  public:
+    HumitureDHT11(int pin):dht(pin, DHT11) {
+      inputPin = pin;
+      pinMode(inputPin, INPUT);
+      dht.begin();
+    }
+
+    Humiture readSensor() {
+      float humidity = dht.readHumidity();
+      float temperature = dht.readTemperature();
+      humiture.humidity = humidity;
+      humiture.temperature = temperature;
+      if (isnan(humidity)) {
+        Serial.println("Failed to read humidity from DHT sensor!");
+        humiture.humidity = -1;
+      }
+      if (isnan(temperature)) {
+        Serial.println("Failed to read temperature from DHT sensor!");
+        humiture.temperature = -1;
+      }
+      return humiture;
+    }
+
+  private:
+    int inputPin;
+    DHT dht;
+    Humiture humiture;
+};
 
 class TempratureLM35 {
   public:
@@ -10,7 +46,7 @@ class TempratureLM35 {
 
     float readSensor() {
       int val = analogRead(inputPin);
-      float temp = (val*0.0048828125*100); 
+      float temp = (val * 0.0048828125 * 100);
       return temp;
     }
 
@@ -25,14 +61,14 @@ class FlameSensor {
       threshold = t;
       pinMode(inputPin, INPUT);
     }
-    
+
     int readSensor() {
       return analogRead(inputPin);
     }
-    
+
     boolean isOverThreshold() {
       int val = readSensor();
-      return val >= threshold;  
+      return val >= threshold;
     }
   private:
     int inputPin;
@@ -40,57 +76,54 @@ class FlameSensor {
 };
 
 /**
- * 利用红外三极管探测火焰,检测到则出发红色LED报警
- *  
- *  - LM35 信号接A0 
- *  - Flame Sensor 正极(长针脚)接电源, 负极(短针脚)接10k电阻, 10k电阻另一端接地,Flame Sensor和10k电阻之间信号接入A1
- *  - 红色LED接入3,蓝色LED接4
- */
+   利用红外三极管探测火焰,检测到则出发红色LED报警
 
-const int PIN_LM35 = A0;
+    - LM35 信号接A0
+    - DHT11 GND接地,VCC接入5v电源,信号接入到PIN 8
+    - Flame Sensor 正极(长针脚)接电源, 负极(短针脚)接10k电阻, 10k电阻另一端接地,Flame Sensor和10k电阻之间信号接入A1
+    - 红色LED接入3,蓝色LED接4
+*/
+
+const int RATE = 2000;
+//const int PIN_LM35 = A0;
 const int PIN_FLAME = A1;
-const int PIN_LED_BLUE = 2;
-const int PIN_LED_RED = 3;
+const int PIN_DHT11 = 8;
 
-LiquidCrystal_I2C lcd(0x3F,16,2); 
-TempratureLM35 lm35(A0);
-FlameSensor fs(A1, 5);
-
-// show alarm in LED 
-void alarm(int pin, int duration) {
-  digitalWrite(pin, HIGH);
-  delay(duration);
-  digitalWrite(pin, LOW);
-}
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+//TempratureLM35 lm35(A0);
+HumitureDHT11 dht11(PIN_DHT11);
+FlameSensor fs(A1, 200);
 
 void setup() {
   // Init LCD
-  lcd.init(); // initialize the lcd 
+  lcd.init(); // initialize the lcd
   lcd.backlight(); //Open the backlight
   lcd.print("Octopus"); // Print a message to the LCD.
-  lcd.setCursor(0,1); //newline
+  lcd.setCursor(0, 1); //newline
   lcd.print("Starting......");// Print a message to the LCD
 
-  pinMode(PIN_LED_BLUE, OUTPUT);
-  pinMode(PIN_LED_RED, OUTPUT);  
-  delay(500);  //dela and wait the sensor ready
+  Serial.begin(9600);
+  delay(1000);  //dela and wait the sensor ready
 }
 
 void loop() {
   lcd.clear();
 
   int fire = fs.readSensor();
-  if (fs.isOverThreshold()) {
-    alarm(PIN_LED_RED, 200);
-  }
-  int temp = lm35.readSensor();
-  
-  lcd.print("F:");
+  //int temp = lm35.readSensor();
+  Humiture h = dht11.readSensor();
+
+  lcd.print("F");
   lcd.print(fire);
-  lcd.print(",T:");
-  lcd.print(temp);
-  lcd.print("");
-  lcd.setCursor(0,1);
+  //lcd.print(" T");
+  //lcd.print(temp);
+  lcd.print(" H");
+  lcd.print((int)h.humidity);
+  lcd.print(",");
+  lcd.print((int)h.temperature);
+
+  lcd.setCursor(0, 1);
   lcd.print("...");
-  delay(1000);
+
+  delay(RATE);
 }
